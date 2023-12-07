dab2b3abe50df1ced49efb1f4d6f40306fbdbc68
Silvina Figueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-12-06 21:32:19 -0800

    Including testsuite

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@
             #REGEX='\bport 63315\b'
     # Patter shouldn't match (egrep -v)
         #a- No match failed logs related to user Kit: 
-            #NEG_REGEX= 'egrep -i failed.*\bkit\b'
+            #NEG_REGEX= 'failed.*\bkit\b'
         #b- Exclude Log error
             #NEG_REGEX='sshd\[[0-9]+\]: error:'
         #c- Unmatch invalid SSH login attemp
@@ -35,18 +35,46 @@
             #NEG_REGEX='systemd-logind\[[0-9]+\]'
         #c - Not include PAM authentication failures
             #NEG_REGEX="PAM( [0-9]+ more)? authentication failure"
- 
 
-failed=0
 
-if egrep -v "$NEG_REGEX" auth.log > auth-log-no-matching.txt;
-then 
-    echo "Sucess"
-else
-    echo "Failed"
-    failed=$((failed + 1)) # increment the counter to track failures 
-fi # end of if statement 
+authlogFile="auth.log"
+gitlogFile="git-log.txt"
 
-echo "Failures tracking: " $failed
+declare -a patterns=(
+    "^[+-] $authlogFile"
+    "\bport 63315\b $authlogFile"
+    "^commit .*tag: $gitlogFile"
+    "Author: Kit Transue $gitlogFile"
+    "sshd.*session opened $authlogFile"
+    "Failed password $authlogFile"
+    "failed.*\bkit\b $authlogFile"
+    "sshd\[[0-9]+\]: error: $authlogFile"
+    "sshd\[[0-9]+\]: Invalid user $authlogFile"
+    "Author:|Date: $gitlogFile"
+    "commit [0-9a-z]{40} \(.*origin/main\) $gitlogFile"
+    "^ {8} $gitlogFile"
+    "sshd\[[0-9]+\] $authlogFile"
+    "systemd-logind\[[0-9]+\] $authlogFile"
+    "PAM( [0-9]+ more)? authentication failure $authlogFile"
+)
 
-exit $failed 
+for item in "${patterns[@]}"; do
+    regex="${item% *}" 
+    file="${item##* }"  
+
+    echo "Testing pattern: $regex in file: $file"
+
+    # Run egrep and -v and store the results to then compare if we have false positives
+    mapfile -t match_results < <(egrep "$regex" "$file")
+    mapfile -t non_match_results < <(egrep -v "$regex" "$file")
+
+    # validating overlaping results
+    for match in "${match_results[@]}"; do
+        for non_match in "${non_match_results[@]}"; do
+            if [ "$match" == "$non_match" ]; then
+                echo "False positive detected: $match"
+            fi
+        done
+    done
+done    
+
